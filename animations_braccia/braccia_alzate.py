134a52a0054f76a61d4da32c1e94de16fb5a6282
alaginestra <antonio.laginestra002@studenti.unibas.it>
2023-09-30 17:49:46 +0200

    Animazioni NAO
    Animazioni delle braccia create su Choregraphe

---
+++ animations_braccia/braccia_alzate.py
@@ -0,0 +1,121 @@
+class BraccioAlzate:
+
+  names = list()
+  times = list()
+  keys = list()
+
+  def get_name(self):
+        return self.names
+
+  def get_time(self):
+        return self.times
+
+  def get_keys(self):
+        return self.keys
+
+  def __init__(self):
+        pass
+
+  names.append("HeadPitch")
+  times.append([0.8])
+  keys.append([[0.263545, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("HeadYaw")
+  times.append([0.8])
+  keys.append([[0.0240091, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("LAnklePitch")
+  times.append([0.8])
+  keys.append([[0.0732452, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("LAnkleRoll")
+  times.append([0.8])
+  keys.append([[-0.0912699, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("LElbowRoll")
+  times.append([0.8])
+  keys.append([[-0.550028, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("LElbowYaw")
+  times.append([0.8])
+  keys.append([[-1.21126, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("LHand")
+  times.append([0.8])
+  keys.append([[1, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("LHipPitch")
+  times.append([0.8])
+  keys.append([[0.173347, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("LHipRoll")
+  times.append([0.8])
+  keys.append([[0.0798263, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("LHipYawPitch")
+  times.append([0.8])
+  keys.append([[-0.159689, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("LKneePitch")
+  times.append([0.8])
+  keys.append([[-0.0905934, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("LShoulderPitch")
+  times.append([0.8])
+  keys.append([[0.434587, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("LShoulderRoll")
+  times.append([0.8])
+  keys.append([[0.205949, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("LWristYaw")
+  times.append([0.8])
+  keys.append([[-1.82387, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("RAnklePitch")
+  times.append([0.8])
+  keys.append([[0.0772572, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("RAnkleRoll")
+  times.append([0.8])
+  keys.append([[0.108743, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("RElbowRoll")
+  times.append([0.8])
+  keys.append([[0.502138, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("RElbowYaw")
+  times.append([0.8])
+  keys.append([[1.29227, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("RHand")
+  times.append([0.8])
+  keys.append([[1, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("RHipPitch")
+  times.append([0.8])
+  keys.append([[0.1608, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("RHipRoll")
+  times.append([0.8])
+  keys.append([[-0.110515, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("RHipYawPitch")
+  times.append([0.8])
+  keys.append([[-0.159689, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("RKneePitch")
+  times.append([0.8])
+  keys.append([[-0.081931, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("RShoulderPitch")
+  times.append([0.8])
+  keys.append([[0.383972, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("RShoulderRoll")
+  times.append([0.8])
+  keys.append([[-0.0331613, [3, -0.28, 0], [3, 0, 0]]])
+
+  names.append("RWristYaw")
+  times.append([0.8])
+  keys.append([[1.61268, [3, -0.28, 0], [3, 0, 0]]])
